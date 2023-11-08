40b4703e187b45598dd05e2b713d0db6a413c5ef
Kelvin George <kelvinpktl@gmail.com>
2023-11-08 19:38:38 +0530

    message

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-fdgkdjnfgj
-// dfkjhd
+var readlineSync = require('readline-sync');
+let a= parseInt(readlineSync.question('enter your first no '));
+let b=readlineSync.question('enter your name')
+let product=a*b
+console.log('product of',a,"and ",b," = ",product);
