6f5a726ea3ce93c93e10ee40235ad5ebfb569f4d
Kelvin George <kelvinpktl@gmail.com>
2023-11-14 19:16:47 +0530

    dfjhgjd

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 var readlineSync = require('readline-sync');
-let a= parseInt(readlineSync.question('enter your first no '));
-let b=readlineSync.question('enter your name')
-let product=a*b
-console.log('product of',a,"and ",b," = ",product);fdgbdf
-ghjgh
-ghjghj
+// let a= parseInt(readlineSync.question('enter your first no '));
+// let b=readlineSync.question('enter your name')
+// let product=a*b
+
+console.log('code start 1');
+let x=10;
+let y =10;
+
+if(x===y ){
+    if(x>20){
+              if(cond 3){
+
+                   }
+    }
+console.log('two nos are equal');
+
+}
+else{
+    console.log('two nos are not equals');
+
+}
+
+(cond1 cond2 cond3) ==>boolean // true /false 
+
+
+console.log('code end  ');
+
+
 
